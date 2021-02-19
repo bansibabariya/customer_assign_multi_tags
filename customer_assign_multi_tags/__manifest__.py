a8ca83fd9d607dec3c28d8d36ba8443c3dc5ee3d
bansibabariya <60510465+bansibabariya@users.noreply.github.com>
2021-02-19 17:09:35 +0530

    Update manifest
    Version change

--- customer_assign_multi_tags/__manifest__.py
+++ customer_assign_multi_tags/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Customer Assign Multi Tags',
     'category': 'Customer',
-    'version': '14.0.1',
+    'version': '13.0.1',
 
     'summary': 'We select multiple customer in tree view and then we will go to action and click "ASSIGN TAGS" and '
                'then open wizard and select one or multiple tags and apply so, reflect tags on which select customer.',
