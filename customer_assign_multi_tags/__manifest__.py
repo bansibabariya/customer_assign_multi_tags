2457158bb333cd0c69615efa047cbe3f8852382a
Dharmesh <drchudasama644@gmail.com>
2021-02-19 17:52:35 +0530

    [ADD]code added for changes

--- customer_assign_multi_tags/__manifest__.py
+++ customer_assign_multi_tags/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Customer Assign Multi Tags',
     'category': 'Customer',
-    'version': '14.0.1',
+    'version': '12.0.1',
 
     'summary': 'We select multiple customer in tree view and then we will go to action and click "ASSIGN TAGS" and '
                'then open wizard and select one or multiple tags and apply so, reflect tags on which select customer.',
@@ -20,7 +20,6 @@ select one or multiple tags and apply so, reflect tags on which select customer.
 
     'depends': ['base', 'web', 'contacts'],
     'data': [
-        'security/ir.model.access.csv',
         'data/data.xml',
         'wizard/assign_tag_wizard.xml',
 
--- customer_assign_multi_tags/models/res_partner.py
+++ customer_assign_multi_tags/models/res_partner.py
@@ -4,17 +4,23 @@ from odoo import models, fields, api, _
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
+    @api.multi
     def action_assign_tags(self):
         no_data = []
         for rec in self.browse(self.env.context['active_ids']):
             no_data.append(rec.id)
         if no_data:
             wizard_id = self.env['assign.tags'].sudo().create({'partner_ids': [(6,0,no_data)]})
+            view = self.env.ref('customer_assign_multi_tags.model_assign_tags_form')
+
             return {
-                'name': _('Assign Tags'),
-                'type': 'ir.actions.act_window',
+                'name': _("Assign Tags"),
                 'view_mode': 'form',
+                'view_id': view.id,
+                'context': {'default_partner_ids': [(6,0,no_data)]},
+                'view_type': 'form',
                 'res_model': 'assign.tags',
-                'res_id': wizard_id.id,
-                'target': 'new'
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+
             }
